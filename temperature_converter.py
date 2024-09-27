498d8a7a8f82eff29f1dbd79e9cee6129162ba9e
diane <villalino.136534100519@depedqc.ph>
2024-09-27 10:58:28 +0800

    Prints an error message in red if the user enters an invalid conversion type

--- temperature_converter.py
+++ temperature_converter.py
@@ -16,3 +16,5 @@ if conversion_type == "C":
 elif conversion_type == "F":
     convert = (temperature * 9 / 5) + 32
     print(f"Your converted temperature is {round(convert, 2)}°F")  # Formula to compute °C to°F
+else:  # Executes if the user input is neither 'C' nor 'F'
+    print("\n\033[0;31mYou entered an invalid choice, please try again.\033[0m")
